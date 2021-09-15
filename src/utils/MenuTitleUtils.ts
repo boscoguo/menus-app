ae67c4f66c7d1a8b7e599d69486a20bcc1df8f6d
bosco.guo <bosco.guo@zerologix.com>
2021-09-16 03:44:54 +1000

    format

--- src/utils/MenuTitleUtils.ts
+++ src/utils/MenuTitleUtils.ts
@@ -1,18 +1,18 @@
 const renderMenuTitle = (data: any) => {
-    const valuesArr = Object.values(data);
-    const obj = { flag: false, title: "" };
-    valuesArr.map(value => {
-        if (value && Array.isArray(value)) {
-            value.map(item => {
-                renderMenuTitle(item);
-                obj.flag = false;
-            })
-        } else {
-            obj.title = data.title;
-            obj.flag = true;
-        }
-    })
-    return obj;
-}
+  const valuesArr = Object.values(data);
+  const obj = { flag: false, title: "" };
+  valuesArr.map(value => {
+    if (value && Array.isArray(value)) {
+      value.map(item => {
+        renderMenuTitle(item);
+        obj.flag = false;
+      });
+    } else {
+      obj.title = data.title;
+      obj.flag = true;
+    }
+  });
+  return obj;
+};
 
 export { renderMenuTitle };
