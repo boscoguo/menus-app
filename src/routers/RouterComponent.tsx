5a23c987163f43f62cbbec5466638e9454ef44da
bosco.guo <bosco.guo@zerologix.com>
2021-09-30 16:27:14 +1000

    format

--- src/routers/RouterComponent.tsx
+++ src/routers/RouterComponent.tsx
@@ -3,13 +3,13 @@ import { LoginPage, CommonPage } from "../pages";
 import { Switch, Route } from "react-router-dom";
 
 const RouterComponent = () => {
-	return (
-		<Switch>
-			<Route exact path="/login" component={LoginPage} />
-			<Route path="/Button-1" component={CommonPage} />
-			<Route path="/Button-3" component={CommonPage} />
-		</Switch>
-	)
-}
+  return (
+    <Switch>
+      <Route exact path="/login" component={LoginPage} />
+      <Route path="/Button-1" component={CommonPage} />
+      <Route path="/Button-3" component={CommonPage} />
+    </Switch>
+  );
+};
 
 export default RouterComponent;
